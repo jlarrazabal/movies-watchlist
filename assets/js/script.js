e2b6c84c03ba7f718d9e75df8eabffc8bcdb0766
Elcin Koyuncu <79725177+ElcinKoyuncu@users.noreply.github.com>
2021-04-17 09:10:50 -0500

    adding files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,89 @@
+//Global Variables
+const omdbKey = "5d0b46f6";
+var movieName = "";
 
+
+var getMovies = function(movie) {
+  let searchedMovie = movie.replace(/\s/g, "+");
+  let sUrl = "https://www.omdbapi.com/?apikey=" + omdbKey + "&s=" + searchedMovie;
+  console.log(sUrl);
+  fetch(sUrl).then(function(response) {
+    if (!response.ok) {
+      $(function() {
+        $("#error").dialog();
+      });
+      console.log(response);
+    } else {
+      return response.json();
+    }
+  }).then(function(data) {
+    console.log(data);
+    for (var i = 0; i < data.Search.length; i++) {
+      let newMovie = $("<li>");
+      let newMovieText= $("<span>")
+      newMovieText.text(data.Search[i].Title)
+      newMovie.attr("data-imdbID", data.Search[i].imdbID);
+      newMovie.addClass("modal-movie-list-item");
+      newMovie.delegate(newMovieText, "click", function(event) {
+        let id = $(event.target).parent().attr("data-imdbID");
+        console.log(id);
+        getMovieDetails(id);
+      });
+      newMovieText.appendTo(newMovie);
+      newMovie.appendTo("#movie-modal-content");
+    }
+    $(function() {
+      $("#movies-modal").dialog();
+    });
+  });
+  console.log(searchedMovie);
+  url = "";
+}
+
+var getMovieDetails = function(id) {
+  console.log(id);
+  let iUrl = "https://www.omdbapi.com/?apikey=" + omdbKey + "&i=" + id;
+  console.log(iUrl);
+  fetch(iUrl).then(function(response) {
+    if (!response.ok) {
+      $(function() {
+        $("#error").dialog();
+      });
+      console.log(response);
+    } else {
+      return response.json();
+    }
+  }).then(function(data) {
+    console.log(data);
+    renderMovieInfor(data);
+  });
+  $("#movies-modal").dialog("close");
+  $("#movie-modal-content").empty();
+  url = "";
+}
+
+var renderMovieInfor = function(data) {
+  $("#movie-details-poster").attr("src", data.Poster);
+  $("#movieYear").text(data.Year);
+  $("#movieLength").text(data.Runtime);
+  $("#movieGenre").text(data.Genre);
+  $("#movieCast").text(data.Actors);
+  $("#movieDirector").text(data.Director);
+  $("#movieProducer").text(data.Production);
+  $("#movieRating").text(data.imdbRating + "/10");
+  $("#movieID").text(data.imdbID);
+}
+
+$("#search-form").delegate("#search-btn", "click", function(event) {
+  event.preventDefault()
+  let movie = $("#movie-search").val();
+  getMovies(movie);
+  $("#movie-search").val("");
+});
+
+$("#search-form").on("submit", function(event) {
+  event.preventDefault()
+  let movie = $("#movie-search").val();
+  getMovies(movie);
+  $("#movie-search").val("");
+});
